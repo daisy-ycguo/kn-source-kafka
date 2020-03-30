234fb414771d4598a2ee51afc2efd8b4436f1866
Daisy Guo <guoyingc@cn.ibm.com>
2020-03-30 23:00:11 +0800

    add unit tests to factories

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ require (
 	github.com/maximilien/kn-source-pkg v0.1.0
 	github.com/spf13/cobra v0.0.6
 	github.com/spf13/pflag v1.0.5
+	gotest.tools v2.2.0+incompatible
 	k8s.io/apimachinery v0.17.0
 	knative.dev/client v0.13.0
 	knative.dev/eventing-contrib v0.13.2
---
+++ pkg/factories/rune_factory_test.go
@@ -0,0 +1,67 @@
+// Copyright © 2020 The Knative Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package factories
+
+import (
+	"testing"
+
+	"github.com/daisy-ycguo/kn-source-kafka/pkg/types"
+	"github.com/spf13/cobra"
+	"gotest.tools/assert"
+)
+
+func TestNewKafkaSourceRunEFactory(t *testing.T) {
+	runEFactory := createKafkaSourceRunEFactory()
+
+	assert.Assert(t, runEFactory != nil)
+}
+
+func TestRunEFactory_KafkaSourceParams(t *testing.T) {
+	runEFactory := createKafkaSourceRunEFactory()
+
+	assert.Assert(t, runEFactory.KafkaSourceFactory().KnSourceParams() != nil)
+	assert.Assert(t, runEFactory.KafkaSourceFactory().KafkaSourceParams() != nil)
+}
+
+func TestRunEFactory_KafkaSourceFactory(t *testing.T) {
+	runEFactory := createKafkaSourceRunEFactory()
+
+	assert.Assert(t, runEFactory.KnSourceFactory() != nil)
+	assert.Assert(t, runEFactory.KafkaSourceFactory() != nil)
+}
+
+func TestRunEFactory_KafkaSourceClient(t *testing.T) {
+	runEFactory := createKafkaSourceRunEFactory()
+	command := &cobra.Command{}
+	params := runEFactory.KafkaSourceFactory().CreateKafkaSourceParams()
+	params.KnSourceParams.AddCommonFlags(command)
+
+	knSourceClient, err := runEFactory.KafkaSourceClient(command)
+	assert.NilError(t, err)
+	assert.Assert(t, knSourceClient != nil)
+}
+
+func TestCreateRunE(t *testing.T) {
+	runEFactory := createKafkaSourceRunEFactory()
+	function := runEFactory.CreateRunE()
+	assert.Assert(t, function != nil)
+}
+
+// Private
+
+func createKafkaSourceRunEFactory() types.KafkaSourceRunEFactory {
+	factory := NewKafkaSourceFactory()
+	return NewKafkaSourceRunEFactory(factory)
+}
